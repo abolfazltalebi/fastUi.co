614372daf811db0e50c03791ae9e096c041dbebc
Your Name <you@example.com>
2025-07-11 18:07:29 +0330

    refactor: update font family in App.css, remove unused image, and enhance Home component with service icons

--- src/assets/components/Icon/Costume.jsx
+++ src/assets/components/Icon/Costume.jsx
@@ -9,7 +9,7 @@ export default function Costume() {
       xmlns="http://www.w3.org/2000/svg"
       class="w-6 h-6 relative"
       preserveAspectRatio="none"
-    >
+    > 
       <path
         d="M7 9L9.53354 10.5201C10.7287 11.2372 10.8423 12.9262 9.75394 13.7968L7 16M12 16H17M6 21H18C19.6569 21 21 19.6569 21 18V6C21 4.34315 19.6569 3 18 3H6C4.34315 3 3 4.34315 3 6V18C3 19.6569 4.34315 21 6 21Z"
         stroke="#252432"
--- src/assets/components/Icon/WebDesign.jsx
+++ src/assets/components/Icon/WebDesign.jsx
@@ -10,7 +10,7 @@ export default function WebDesign() {
       xmlns="http://www.w3.org/2000/svg"
       class="flex-grow-0 flex-shrink-0 w-6 h-6 relative"
       preserveAspectRatio="none"
-    >
+    > 
       <path
         d="M12 4V5M12 19V20M5.07178 8L5.9378 8.5M18.0622 15.5L18.9282 16M18.9282 7.99999L18.0622 8.49999M5.93781 15.5L5.07178 16M16 12C16 9.79086 14.2091 8 12 8C9.79084 8 7.99998 9.79086 7.99998 12C7.99998 14.2091 9.79084 16 12 16C14.2091 16 16 14.2091 16 12Z"
         stroke="#252432"
